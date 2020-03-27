9a796402fed04d4ac1e017b40618d505a225e11f
Frankie96 <frankie_96@windowslive.com>
2020-03-27 18:24:52 +0100

    new color

--- src/components/categories.js
+++ src/components/categories.js
@@ -1,8 +1,8 @@
 module.exports = [
 	{ ident: 'childcare', text: 'Kinderbetreuung', color: '#1289A7' },
-	{ ident: 'food', text: 'Food Delivery', color: '#D980FA' },
+	{ ident: 'food', text: 'Lieferservice', color: '#D980FA' },
 	{ ident: 'takeaway', text: 'Food Take-Away', color: '#30336b' },
-	{ ident: 'shop', text: 'Happy to shop for you', color: '#8e44ad' },
+	{ ident: 'shop', text: 'Einkaufen gehen', color: '#8e44ad' },
 	{ ident: 'information', text: 'Unterhaltung', color: '#2980b9' },
 	{ ident: 'medical', text: 'Medizinische Hilfe', color: '#d35400' },
 	{ ident: 'petcare', text: 'Huastier Betreuung', color: '#e1b12c' },
