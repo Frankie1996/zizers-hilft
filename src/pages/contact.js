b112de0a8cc864b327b4b2062f8315f27a98c516
Frankie96 <frankie_96@windowslive.com>
2020-03-31 20:36:46 +0200

    br

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -15,7 +15,7 @@ const Contact = () => {
               Über uns
             </span>
               <div className='sub header'>
-                Das ursprüngliche Projekt wurde vom Datenjournalisten beim Tagesanzeiger, Simon Huwiler, entwickelt und durch <a href={'mailto:marc.fehr@gmail.com'}>Marc Fehr</a>  weiterentwickelt. Dieser stellte seinen Code als Open Source Repository auf <a href='https://gitlab.com/marc.fehr/community-isolation-map' target="_blank">Gitlab</a> zur verfügbar. Weiterentwickelt wurde das Projekt durch
+                Das ursprüngliche Projekt wurde vom Datenjournalisten beim Tagesanzeiger, Simon Huwiler, entwickelt und durch <a href={'mailto:marc.fehr@gmail.com'}>Marc Fehr</a>  weiterentwickelt. Dieser stellte seinen Code als Open Source Repository auf <a href='https://gitlab.com/marc.fehr/community-isolation-map' target="_blank">Gitlab</a> zur verfügbar. <br/><br/>Weiterentwickelt wurde das Projekt durch
               <ul>
                 <li><a href="mailto:frank.zinsli@gmail.com">Frank Zinsli - (frank.zinsli@gmail.com)</a><ul><li> #ZizersHilftZizers </li></ul></li>
                 <li><a href="mailto:beni.aebersold@gmail.com">Bernhard Aebersold</a> <ul><li><a href="https://churhilftchur.ch/" target="_blank">Chur hilft Chur</a> #ChurHilftChur</li></ul></li>
